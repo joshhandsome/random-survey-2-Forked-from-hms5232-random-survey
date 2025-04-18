a21fd1f852a887b50339d41c09dbca1f12db26fb
joshhandsome <144260521+joshhandsome@users.noreply.github.com>
2025-04-18 16:21:16 +0800

    Create url.js

---
+++ url.js
@@ -0,0 +1,16 @@
+/*
+ * 將要隨機派發的網址放入下方（不需排序）
+ *
+ * 並請注意：
+ * 1. 網址請用引號（或稱「撇號」，單引號或雙引號皆可）包起來
+ * 2. 包起來的網址之間用逗號分隔
+ */
+
+
+const urls = [
+	'https://www.surveycake.com/s/pM8xV',
+	'https://www.surveycake.com/s/abkzp',
+	'https://www.surveycake.com/s/66koq',
+	'https://www.surveycake.com/s/zxMPo',
+	
+];
